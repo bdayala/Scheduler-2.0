2b9e17bedc7f8883f39effe0a8efdb547adadbe5
myname <myname>
2017-11-30 23:25:34 -0600

    had trouble pushing into repository, copied files to new folder

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+  	return role_id == 99
+  end
+
+  def user
+  	return role_id == 90
+  end
+
+end
+
+
